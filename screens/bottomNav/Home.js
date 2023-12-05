ed73d9741c2788d43f721bfc482005a70d1a1c00
Armin Barzegar Amin <armin.barzegaramin@student.ap.be>
2023-12-05 09:38:03 +0100

    Feat: Like feature with SVG added

--- screens/bottomNav/Home.js
+++ screens/bottomNav/Home.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, Text, View, LogBox, ScrollView, Button, TouchableOpacity } from 'react-native';
 
+import LikeIconSvg from '../../assets/Like.svg'
 
 import useNetwork from '../../data/network'
 
+
 export default function Home({ navigation }) {
-
   //dbData = ()=>{fetch('http://2a02:1810:c31:5800:69b5:3593:275a:d2fb/endpoint.php')} 
-
-  const axearminimage = require('../../assets/axearmin.png'); //constante voor foto
-  const starsvg = require('../../assets/star.png'); //constante voor foto
 
   const likeIcon = require('../../assets/Like.png');
   const commentIcon = require('../../assets/Comment.png');
   const shareIcon = require('../../assets/Share.png');
-  
 
+  const likeIconSvg = require('../../assets/Like.svg')
+
+  const [likeColor, setLikeColor] = useState('white'); // Initial color of LikeIconSvg
+
+  const handleLikePress = () => {
+    // Toggle the color when LikeIconSvg is pressed
+    const newColor = likeColor === 'white' ? 'red' : 'white';
+    setLikeColor(newColor);
+
+    // You can perform additional actions here when the LikeIconSvg is pressed
+  };
 
   const {network, isLoading, isError} = useNetwork()
   console.log(network);
@@ -34,12 +42,15 @@ export default function Home({ navigation }) {
       </TouchableOpacity>
 
       <View style={styles.interactives}>
-        <Image
-          style={{width:20, height:19,
-            marginHorizontal: 0,
-          }}
-          source={likeIcon}
-        />
+
+        <TouchableOpacity onPress={handleLikePress}>
+          <LikeIconSvg
+            width={20}
+            height={19}
+            fill={likeColor} // Set the fill color dynamically based on state
+          />
+        </TouchableOpacity>
+
         <Image
           style={{width:20, height:20,
             marginHorizontal: 12,}}
