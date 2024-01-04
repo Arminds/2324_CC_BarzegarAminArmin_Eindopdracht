e1848692bef1c0133c3fc1650be413902f708d1a
Armin Barzegar Amin <armin.barzegaramin@student.ap.be>
2024-01-04 16:02:22 +0100

    add: images to profile and scrollview

--- screens/bottomNav/Profile.js
+++ screens/bottomNav/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View, ScrollView } from 'react-native';
 import { useFonts, Inter_900Black, Inter_700Bold, Inter_400Regular, Inter_300Light } from '@expo-google-fonts/inter';
 
 import * as authService from '../../components/authService';
@@ -49,59 +49,101 @@ export default function Profile({ navigation, route }) {
   console.log("Following Count:", following);
 
   const axearminimage = require('../../assets/axearmin.png');
+
+  const ai1 = require('../../assets/aiImages/1.jpeg');
+  const ai2 = require('../../assets/aiImages/2.jpeg');
+  const ai3 = require('../../assets/aiImages/3.jpeg');
+  const ai4 = require('../../assets/aiImages/4.jpeg');
+  const ai5 = require('../../assets/aiImages/5.jpeg');
+  const ai6 = require('../../assets/aiImages/6.jpeg');
+  const ai7 = require('../../assets/aiImages/7.jpeg');
+  const ai8 = require('../../assets/aiImages/8.jpeg');
+
   const starIcon = require('../../assets/star.png');
 
   const BASE_URL = 'https://armin.vaw.be/';
 
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.profilePicture}
-        source={{ uri: BASE_URL + profileImage }}
-      />
-      <Text style={styles.header}>{username}</Text>
-      <View style={styles.rank}>
-        <View style={styles.gridRank}>
-          <Image
-            style={styles.iconRank}
-            source={starIcon}
-          />
+    <ScrollView style={styles.scrollView} contentContainerStyle={styles.scrollViewContainer}>
+      <View style={styles.container}>
+        <Image
+          style={styles.profilePicture}
+          source={{ uri: BASE_URL + profileImage }}
+        />
+        <Text style={styles.header}>{username}</Text>
+        <View style={styles.rank}>
+          <View style={styles.gridRank}>
+            <Image
+              style={styles.iconRank}
+              source={starIcon}
+            />
+          </View>
+          <View style={styles.gridRank}>
+            <Text style={styles.body}>{rank}</Text>
+          </View>
         </View>
-        <View style={styles.gridRank}>
-          <Text style={styles.body}>{rank}</Text>
+        <View style={styles.profileInfo}>
+          <View>
+            <View style={styles.gridItem}>
+              <Text style={styles.body}>32</Text>
+              <Text style={styles.body2}>Blogs</Text>
+            </View>
+          </View>
+          <View style={styles.Divider}></View>
+          <View>
+            <View style={styles.gridItem}>
+              <Text style={styles.body}>{followers}</Text>
+              <Text style={styles.body2}>Followers</Text>
+            </View>
+          </View>
+          <View style={styles.Divider}></View>
+          <View>
+            <View style={styles.gridItem}>
+              <Text style={styles.body}>{following}</Text>
+              <Text style={styles.body2}>Following</Text>
+            </View>
+          </View>
+        </View>
+        <StatusBar style="light" />
+        <View style={styles.containerImage}>
+          <Image style={styles.image2} source={ai1}/>
+          <Image style={styles.image2} source={ai2}/>
+          <Image style={styles.image2} source={ai3}/>
+          <Image style={styles.image2} source={ai4}/>
+          <Image style={styles.image2} source={ai5}/>
+          <Image style={styles.image2} source={ai6}/>
+          <Image style={styles.image2} source={ai7}/>
+          <Image style={styles.image2} source={ai8}/>
+          <Image style={styles.image2} source={ai1}/>
+          <Image style={styles.image2} source={ai2}/>
+          <Image style={styles.image2} source={ai3}/>
+          <Image style={styles.image2} source={ai4}/>
+          <Image style={styles.image2} source={ai5}/>
+          <Image style={styles.image2} source={ai6}/>
+          <Image style={styles.image2} source={ai7}/>
+          <Image style={styles.image2} source={ai8}/>          
+          <Image style={styles.image2} source={ai1}/>
+          <Image style={styles.image2} source={ai2}/>
+          <Image style={styles.image2} source={ai3}/>
+          <Image style={styles.image2} source={ai4}/>
+          <Image style={styles.image2} source={ai5}/>
+          <Image style={styles.image2} source={ai6}/>
+          <Image style={styles.image2} source={ai7}/>
+          <Image style={styles.image2} source={ai8}/>
+
+
         </View>
       </View>
-      <View style={styles.profileInfo}>
-        <View>
-          <View style={styles.gridItem}>
-            <Text style={styles.body}>32</Text>
-            <Text style={styles.body2}>Blogs</Text>
-          </View>
-        </View>
-        <View style={styles.Divider}></View>
-        <View>
-          <View style={styles.gridItem}>
-            <Text style={styles.body}>{followers}</Text>
-            <Text style={styles.body2}>Followers</Text>
-          </View>
-        </View>
-        <View style={styles.Divider}></View>
-        <View>
-          <View style={styles.gridItem}>
-            <Text style={styles.body}>{following}</Text>
-            <Text style={styles.body2}>Following</Text>
-          </View>
-        </View>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: '#000000', // Set the background color for the ScrollView
+  },
   container: {
     flex: 1,
-    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'top',
     padding: 24,
@@ -114,6 +156,20 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     borderRadius: 100,
     backgroundColor: 'grey',
+  },
+  containerImage: {
+    width: '100%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginTop: 24,
+  },
+  image2: {
+    width: '32%',
+    height: '30%',
+    margin: 2,
+    aspectRatio: 1,
+    backgroundColor: 'darkgrey',
   },
   header: {
     color: '#FFF',
@@ -150,6 +206,7 @@ const styles = StyleSheet.create({
   },
   profileInfo: {
     flexDirection: 'row',
+    marginLeft: 20,
   },
   gridItem: {
     marginHorizontal: 24,
@@ -158,5 +215,10 @@ const styles = StyleSheet.create({
     width: 1,
     height: '100%',
     backgroundColor: '#2B2B2B',
+  },
+  scrollViewContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    paddingBottom: 24,
   },
 });
